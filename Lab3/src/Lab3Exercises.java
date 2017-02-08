c3f6db8605a31a28b51be98349e145f835ec299e
4015c12 <4015c12@carambola.ece.uprm.edu>
2017-02-08 12:41:58 -0400

    Added MyPanelClass Changed background color Set frame to center of screen Added 2 borders, one inner and one outer

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -1,11 +1,19 @@
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Christian Perez Villanueva");
-		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(850,400);
+		Dimension centerWindow = Toolkit.getDefaultToolkit().getScreenSize();
 		myFrame.setSize(200, 200);
+		int width = (int) ((centerWindow.getWidth() - myFrame.getWidth()) / 2);
+	    int height = (int) ((centerWindow.getHeight() - myFrame.getHeight()) / 2);
+	    myFrame.setLocation(width, height);
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		MyPanelClass myPanel = new MyPanelClass();
+        myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
 	}
 }
